8781fb2f832ba99ead6476acc0b4f56744a9ef11
Kota Suzuki <addressuzuki@gmail.com>
2024-02-08 20:56:50 +0900

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by 鈴木航太 on 2024/02/08.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
