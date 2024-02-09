26e8bdd1c5972300dd19f8d7260db499308e12f3
Kota Suzuki <addressuzuki@gmail.com>
2024-02-09 17:27:26 +0900

    ダブルタップで拡大縮小機能追加、offsetの可視化

---
+++ Pinch/Screen/ContentView.swift
@@ -0,0 +1,87 @@
+//
+//  ContentView.swift
+//  Pinch
+//
+//  Created by 鈴木航太 on 2024/02/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var isAnimating: Bool = false
+    @State private var imageScale: CGFloat = 1
+    @State private var imageOffset: CGSize = .zero
+    
+    
+    func resetImageState() {
+        return withAnimation(.spring()) {
+            imageScale = 1
+            imageOffset = .zero
+        
+        }
+    }
+    var body: some View {
+        NavigationView {
+            ZStack {
+                Color.clear
+                // MARK: - PAGE IMAGE
+                Image("magazine-front-cover")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(10)
+                    .padding()
+                    .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+                    .opacity(isAnimating ? 1 : 0)
+                    .offset(x: imageOffset.width, y: imageOffset.height)
+                    .scaleEffect(imageScale)
+                    .onTapGesture(count: 2) {
+                        if imageScale == 1 {
+                            withAnimation(.spring()) {
+                                imageScale = 5
+                            }
+                        } else {
+                            withAnimation(.spring()) {
+                                resetImageState()
+                            }
+                        }
+                    }
+                    .gesture(
+                    DragGesture()
+                        .onChanged{ value in
+                            withAnimation(.linear(duration: 1)) {
+                                imageOffset = value.translation
+                            }
+                        }
+                        .onEnded({ _ in
+                            if imageScale <= 1 {
+                                resetImageState()
+                            }
+                        })
+                    )
+                
+            }
+            .navigationTitle("Pinch & Zoom")
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear {
+                withAnimation(.linear(duration: 1)) {
+                    isAnimating = true
+                }
+            }
+            // MARK: INFO PANEL
+            .overlay(
+                InfoPanelView(scale: imageScale, offset: imageOffset)
+                    .padding(.horizontal)
+                    .padding(.top, 30)
+                , alignment: .top
+            )
+        } // NAVIGATION
+        .navigationViewStyle(.stack)
+    }
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
